4aede2a34b54d26313c851c1fbda26c52e5807ce
H1710 <huyquac@gmail.com>
2023-04-20 23:37:34 +0700

    2

--- back-end/index.js
+++ back-end/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
-const DBconnection = require('./DBconnection/db');
-const routes = require('./routes/index');
-const { socketConnect } = require('./models/socket');
+const DBconnection = require('./src/DBconnection/db');
+const routes = require('./src/routes/index');
+const { socketConnect } = require('./src/models/socket');
 require('dotenv').config();
 app.use(cookieParser());
 
