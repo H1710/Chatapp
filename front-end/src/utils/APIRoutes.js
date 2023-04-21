ea6b005303dced0fd880327911b13bcb82fe77a2
H1710 <huyquac@gmail.com>
2023-04-21 16:49:04 +0700

    1

--- front-end/src/utils/APIRoutes.js
+++ front-end/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = 'https://chat-app-be-three.vercel.app';
+export const host = 'https://chat-app-be-three.vercel.app/api/v1';
 
 export const registerRoute = `${host}/user/register`;
 export const avatarRoute = `${host}/upload`;
