553d10727a56b85335ca35e0faa3337c117d540f
H1710 <huyquac@gmail.com>
2023-04-21 15:53:27 +0700

    1

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const DBconnection = require('./src/DBconnection/db');
 const routes = require('./src/routes/index');
 const { socketConnect } = require('./src/models/socket');
 require('dotenv').config();
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.use(
   cors({ credentials: true, origin: 'https://chat-app-fe-ruddy.vercel.app' })
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 
 DBconnection();
 
-app.use('/', routes);
+app.use('/api/v1', routes);
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on Port ${process.env.PORT}`);
 });
 
-// socketConnect(server);
+socketConnect(server);
