7c2f26699d224e10475b33d619786c59650b18a2
H1710 <huyquac@gmail.com>
2023-04-26 03:29:00 +0700

    test

--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -21,24 +21,39 @@ function Chat() {
 
   const { auth } = useSelector(state => state);
 
-  useEffect(() => {
-    const handleHome = async () => {
-      const chat_app_key = await localStorage.getItem('chat-app');
-      if (chat_app_key === 'fe1') {
-        socket.current = io('https://chat-app-be1.onrender.com/');
-        dispatch({ type: 'SOCKET', payload: socket.current });
-        socket.current.emit('login', { userId: auth._id });
-        socket.current.on('onlineUser', data => {
-          const usersId = Object.values(data.onlineUsers);
-          setOnlineUsers(usersId);
-          setOfflineUsersTime(data.offlineUsersTime);
-        });
-      } else {
-        await navigate('/login');
-      }
-    };
-    handleHome();
-  }, [auth]);
+  // useEffect(() => {
+  //   const handleHome = async () => {
+  //     const chat_app_key = await localStorage.getItem('chat-app');
+  //     if (chat_app_key === 'fe1') {
+  //       socket.current = io('https://chat-app-be1.onrender.com/');
+  //       dispatch({ type: 'SOCKET', payload: socket.current });
+  //       socket.current.emit('login', { userId: auth._id });
+  //       socket.current.on('onlineUser', data => {
+  //         const usersId = Object.values(data.onlineUsers);
+  //         setOnlineUsers(usersId);
+  //         setOfflineUsersTime(data.offlineUsersTime);
+  //       });
+  //     } else {
+  //       await navigate('/login');
+  //     }
+  //   };
+  //   handleHome();
+  // }, [auth]);
+  useLayoutEffect(() => {
+    if (auth.access_token) {
+      socket.current = io('https://chat-app-be1.onrender.com/');
+      dispatch({ type: 'SOCKET', payload: socket.current });
+      socket.current.emit('login', { userId: auth._id });
+      socket.current.on('onlineUser', data => {
+        const usersId = Object.values(data.onlineUsers);
+        // console.log(usersId);
+        setOnlineUsers(usersId);
+        setOfflineUsersTime(data.offlineUsersTime);
+      });
+    } else {
+      navigate('/login');
+    }
+  }, [auth.access_token]);
 
   // useEffect(() => {
   //   const checkUser = async () => {
