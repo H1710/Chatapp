537feb30ebacda056a2d511acc9124377b1c0b7b
H1710 <huyquac@gmail.com>
2023-09-22 17:51:16 +0700

    fix cookie

--- back-end/src/controllers/auth.js
+++ back-end/src/controllers/auth.js
@@ -53,6 +53,8 @@ class AuthController {
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
         sameSite: 'none',
+        domain: 'https://chat-app-fe-jdt5.onrender.com',
+        secure: true,
       });
 
       return res.status(200).send({
